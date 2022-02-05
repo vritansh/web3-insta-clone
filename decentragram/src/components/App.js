9bd41df20d9a41dbd075bd2508e66bde2bd81bce
vritansh <vritansh14@gmail.com>
2022-02-05 20:15:22 +0530

    added crypto wallet

--- decentragram/src/components/App.js
+++ decentragram/src/components/App.js
@@ -9,6 +9,41 @@ import Main from './Main'
 
 class App extends Component {
 
+  async componentDidMount() {
+     await this.loadWeb3();
+     await this.loadBlockChainData();
+  }
+
+
+  async loadWeb3()
+  {
+      if(window.etherium) {
+        window.web3 = new Web3(window.etherium)
+        await window.etherium.enabe()
+      } else if(window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider)
+      } else {
+          window.alert('Non etherium browser detected. You should consider trying meta mask');
+      }
+  }
+  
+async loadBlockChainData() {
+  const web3 = window.web3;
+  //load account
+  const accounts = await web3.eth.getAccounts(); 
+  this.setState({account: accounts[0]})
+  //Network Id 
+  const networkId = await web3.eth.net.getId()
+  const networkData = Decentragram.networks[networkId]
+  console.log(networkData)
+  if(networkData){
+    const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+  }else{
+    window.alert("decentragram contract not deploye to detected network")
+  }
+
+}
+
   constructor(props) {
     super(props)
     this.state = {
@@ -23,7 +58,6 @@ class App extends Component {
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-            // Code...
             />
           }
         }
--- decentragram/src/components/Navbar.js
+++ decentragram/src/components/Navbar.js
@@ -4,7 +4,7 @@ import photo from '../photo.png'
 
 class Navbar extends Component {
 
-  render() {
+  render(props) {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
@@ -19,7 +19,7 @@ class Navbar extends Component {
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{'0x0'}</small>
+              <small id="account">{this.props.account}</small>
             </small>
             { this.props.account
               ? <img
